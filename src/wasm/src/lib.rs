375d937be8c60539b13b9f5074fb620c0c98cb38
hazae41 <4405263+hazae41@users.noreply.github.com>
2025-06-27 13:27:26 +0900

    d

--- src/wasm/src/lib.rs
+++ src/wasm/src/lib.rs
@@ -1,17 +1,4 @@
+mod libs;
 mod mods;
 
 pub use mods::*;
-
-#[macro_export]
-macro_rules! rjse {
-    ($x:expr) => {
-        $x.map_err(|e| JsError::new(&format!("{} at {}", e, std::any::type_name_of_val(&|| {}))))
-    };
-}
-
-#[macro_export]
-macro_rules! ojse {
-    ($x:expr) => {
-        $x.ok_or_else(|| JsError::new(format!("Option was None at {}", std::any::type_name_of_val(&|| {}))))
-    };
-}
