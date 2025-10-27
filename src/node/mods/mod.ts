8e0f44afacb3b0b84a6886b4b81fd70267bd1f46
hazae41 <4405263+hazae41@users.noreply.github.com>
2025-10-27 22:06:52 +0700

    d

--- src/node/mods/mod.ts
+++ src/node/mods/mod.ts
@@ -1,9 +1,15 @@
 export * from "../../wasm/pkg/argon2_wasm.js";
 
 /* @ts-types="../../wasm/pkg/argon2_wasm.d.ts" */
-import init, { type InitOutput } from "../../wasm/pkg/argon2_wasm.js";
+import init, {
+  initSync, type InitOutput
+} from "../../wasm/pkg/argon2_wasm.js";
 import { data } from "../../wasm/pkg/argon2_wasm.wasm.js";
 
-export async function initBundled(): Promise<InitOutput> {
+export async function load(): Promise<InitOutput> {
   return await init({ module_or_path: data })
+}
+
+export function loadSync(): InitOutput {
+  return initSync({ module_or_path: data });
 }
